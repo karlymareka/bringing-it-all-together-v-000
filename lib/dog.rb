791dc99dec3e939cf5d429e70e6392c4a2b90d81
Karly Mareka <karly.mareka@gmail.com>
2019-04-06 03:32:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ attr_accessor :name, :breed, :id
     dog = DB[:conn].execute(sql, dog_info[:name], dog_info[:breed])[0]
     binding.pry 
     if dog != nil
-      dog_info[:id]
+      dog_info[:id] = dog[0]
       dog = Dog.new(dog)
     else
       dog = Dog.create(dog_info)
