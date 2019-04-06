957fc192d954810ae9dfcc3626a37adefc228e6c
Karly Mareka <karly.mareka@gmail.com>
2019-04-06 03:36:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,6 +66,6 @@ attr_accessor :name, :breed, :id
   
   def self.new_from_db(dog)
     dog_info = {:id => dog[0], :name => dog[1], :breed => dog[2]}
-    new_dog = Dog.new 
+    new_dog = Dog.new(dog_info) 
   end 
 end 
